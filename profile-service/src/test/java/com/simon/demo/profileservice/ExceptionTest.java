7e7c7f6d9e2a26ec73f52db84dc32f0a1d404c83
Simon <simon.w.w.zhang@hsbc.com.cn>
2018-05-29 15:06:36 +0800

    commit

--- profile-service/src/test/java/com/simon/demo/profileservice/ExceptionTest.java
+++ profile-service/src/test/java/com/simon/demo/profileservice/ExceptionTest.java
@@ -2,6 +2,9 @@ package com.simon.demo.profileservice;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class ExceptionTest {
 
     @Test
@@ -17,5 +20,25 @@ public class ExceptionTest {
             System.out.println("Exception:" + e.getMessage());
         }
 
+    }
+
+    @Test
+    public void testTime(){
+        Long expireValue = 1527559851 * 1000L;
+
+        java.util.Date expiredAt = new java.util.Date(expireValue);
+        Calendar cal = Calendar.getInstance();
+
+
+        //Default has 2 minutes buffer
+        long buffer = 2 * 60 * 1000L;
+
+        if (cal.getTimeInMillis() - expiredAt.getTime() < buffer ) {
+            System.out.println(cal.getTimeInMillis() - expiredAt.getTime());
+        }
+
+        System.out.println(expiredAt.getTime());
+        System.out.println(cal.getTimeInMillis());
+        System.out.println(System.currentTimeMillis());
     }
 }
