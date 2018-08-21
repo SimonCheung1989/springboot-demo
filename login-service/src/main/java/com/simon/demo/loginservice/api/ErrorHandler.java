1cf0c655b6d5dcc869acc7a974c2a98ae97f373a
Simon <simon.w.w.zhang@hsbc.com.cn>
2018-08-21 17:33:35 +0800

    add log

--- login-service/src/main/java/com/simon/demo/loginservice/api/ErrorHandler.java
+++ login-service/src/main/java/com/simon/demo/loginservice/api/ErrorHandler.java
@@ -1,6 +1,8 @@
 package com.simon.demo.loginservice.api;
 
 import com.simon.demo.loginservice.model.ErrorDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,17 +11,20 @@ import org.springframework.web.client.RestClientException;
 @ControllerAdvice
 @ResponseBody
 public class ErrorHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
+
     @ExceptionHandler(feign.RetryableException.class)
     public ErrorDTO handleRetryableException(Exception e) {
-        System.out.println("--------RetryableException----------");
-        e.printStackTrace();
+        log.debug("--------RetryableException----------");
+        log.error(e.getMessage(), e);
         return new ErrorDTO();
     }
 
     @ExceptionHandler(RestClientException.class)
     public ErrorDTO handleRestClientException(Exception e) {
-        System.out.println("--------RestClientException----------");
-        e.printStackTrace();
+        log.debug("--------RestClientException----------");
+        log.error(e.getMessage(), e);
         return new ErrorDTO();
     }
 }
