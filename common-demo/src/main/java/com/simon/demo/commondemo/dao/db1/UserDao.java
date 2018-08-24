65f9fbfb5ab0806d170e96c2b9c1cc7da7087e47
Simon <simon.w.w.zhang@hsbc.com.cn>
2018-08-24 19:18:47 +0800

    enable database

--- common-demo/src/main/java/com/simon/demo/commondemo/dao/db1/UserDao.java
+++ common-demo/src/main/java/com/simon/demo/commondemo/dao/db1/UserDao.java
@@ -2,8 +2,16 @@ package com.simon.demo.commondemo.dao.db1;
 
 import com.simon.demo.commondemo.entities.db1.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserDao extends JpaRepository<UserEntity, Integer> {
+
+    @Query(value = "select * from T_USER where NAME like :name% order by NAME limit :offset, :limit",
+            nativeQuery = true)
+    List<UserEntity> findByName(@Param("name") String name, @Param("offset") Integer offset, @Param("limit") Integer limit);
 }
--- common-demo/src/test/java/com/simon/demo/commondemo/CommonDemoApplicationTests.java
+++ common-demo/src/test/java/com/simon/demo/commondemo/CommonDemoApplicationTests.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CommonDemoApplicationTests {
@@ -29,11 +31,17 @@ public class CommonDemoApplicationTests {
 	}
 
 	@Test
-	public void testUserDao(){
-		UserEntity userEntity =  new UserEntity();
+	public void testUserDao() throws Exception{
+
 //		userEntity.setId(1);
-		userEntity.setName("Simon");
-		userDao.save(userEntity);
+		for(int i=0; i<100; i++) {
+			UserEntity userEntity =  new UserEntity();
+			System.out.println(i);
+			userEntity.setName("Simon" + i);
+			userDao.saveAndFlush(userEntity);
+			Thread.sleep(1000);
+		}
+
 	}
 
 	@Test
@@ -41,6 +49,15 @@ public class CommonDemoApplicationTests {
 		BlogEntity blogEntity = new BlogEntity();
 		blogEntity.setTitle("This is title");
 		blogDao.save(blogEntity);
+	}
+
+	@Test
+	public void testFindByName() {
+		List<UserEntity> list =  userDao.findByName("Simon", 0, 10);
+
+		list.stream().forEach(entity -> {
+			System.out.println(entity.getId());
+		});
 	}
 
 }
