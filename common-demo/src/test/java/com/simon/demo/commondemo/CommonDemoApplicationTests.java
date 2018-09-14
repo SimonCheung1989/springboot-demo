6152f7d3c0e6c6eec3cb87ce0f8ec241808f3445
Simon <simon.w.w.zhang@hsbc.com.cn>
2018-09-14 16:37:09 +0800

    pdf

--- common-demo/src/test/java/com/simon/demo/commondemo/CommonDemoApplicationTests.java
+++ common-demo/src/test/java/com/simon/demo/commondemo/CommonDemoApplicationTests.java
@@ -1,8 +1,28 @@
 package com.simon.demo.commondemo;
 
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import com.simon.demo.commondemo.akka.MasterActor;
 import com.simon.demo.commondemo.akka.WorkerActor;
 import com.simon.demo.commondemo.component.ServiceA;
@@ -12,20 +32,10 @@ import com.simon.demo.commondemo.dao.db2.BlogDao;
 import com.simon.demo.commondemo.entities.db1.UserEntity;
 import com.simon.demo.commondemo.entities.db2.BlogEntity;
 import com.simon.demo.commondemo.model.Notification;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -47,15 +57,15 @@ public class CommonDemoApplicationTests {
 	}
 
 	@Test
-	public void testUserDao() throws Exception{
+	public void testUserDao() throws Exception {
 
 //		userEntity.setId(1);
-		for(int i=0; i<100; i++) {
-			UserEntity userEntity =  new UserEntity();
+		for (int i = 0; i < 100; i++) {
+			UserEntity userEntity = new UserEntity();
 			System.out.println(i);
 			userEntity.setName("Simon" + i);
 			userDao.saveAndFlush(userEntity);
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		}
 
 	}
@@ -69,7 +79,7 @@ public class CommonDemoApplicationTests {
 
 	@Test
 	public void testFindByName() {
-		List<UserEntity> list =  userDao.findByName("Simon", 0, 10);
+		List<UserEntity> list = userDao.findByName("Simon", 0, 10);
 
 		list.stream().forEach(entity -> {
 			System.out.println(entity.getId());
@@ -84,7 +94,7 @@ public class CommonDemoApplicationTests {
 
 	@Test
 	public void testTransactional() {
-		UserEntity userEntity =  new UserEntity();
+		UserEntity userEntity = new UserEntity();
 		userEntity.setName("Transactional4");
 		BlogEntity blogEntity = new BlogEntity();
 		blogEntity.setTitle("Transactional4");
@@ -94,7 +104,7 @@ public class CommonDemoApplicationTests {
 
 	@Test
 	public void testManualTransactional() {
-		UserEntity userEntity =  new UserEntity();
+		UserEntity userEntity = new UserEntity();
 		userEntity.setName("Transactional6");
 		BlogEntity blogEntity = new BlogEntity();
 		blogEntity.setTitle("Transactional6");
@@ -103,17 +113,18 @@ public class CommonDemoApplicationTests {
 	}
 
 	@Test
-	public void testTransfer(){
-		UserEntity user1 =  this.userDao.findById(1).get();
-		UserEntity user2 =  this.userDao.findById(2).get();
+	public void testTransfer() {
+		UserEntity user1 = this.userDao.findById(1).get();
+		UserEntity user2 = this.userDao.findById(2).get();
 		System.out.println(user1.getName());
 		System.out.println(user2.getName());
 
-		for(int i=0; i<10000; i++) {
+		for (int i = 0; i < 100; i++) {
 			Thread thread = new Thread(() -> {
 				serviceA.transfer(user1, user2);
 				serviceA.transfer(user2, user1);
-				System.out.println("User1.score=" + user1.getScore() +", Total=" + (user1.getScore() + user2.getScore()));
+				System.out.println(
+						"User1.score=" + user1.getScore() + ", Total=" + (user1.getScore() + user2.getScore()));
 			});
 			thread.start();
 		}
@@ -129,8 +140,8 @@ public class CommonDemoApplicationTests {
 	}
 
 	@Test
-	public void testAsync(){
-		for(int i=0; i<10000; i++) {
+	public void testAsync() {
+		for (int i = 0; i < 100; i++) {
 			int number = i;
 			Thread thread = new Thread(() -> {
 				serviceA.saveAndSendNotification(number);
@@ -160,7 +171,7 @@ public class CommonDemoApplicationTests {
 		ActorRef workerActor = actorSystem.actorOf(Props.create(WorkerActor.class), "WorkerActor");
 		ActorRef masterActor = actorSystem.actorOf(Props.create(MasterActor.class, workerActor), "MasterActor");
 
-		for(int i=0; i<100; i++) {
+		for (int i = 0; i < 100; i++) {
 			Notification notification = new Notification();
 			notification.setMailbox("simon" + i + "@mail.com");
 			notification.setUserId("Simon" + i);
@@ -168,7 +179,7 @@ public class CommonDemoApplicationTests {
 		}
 		System.out.println("---end---");
 		System.out.println("Enter to exist");
-		try{
+		try {
 			System.in.read();
 		} catch (Exception e) {
 		} finally {
@@ -178,20 +189,20 @@ public class CommonDemoApplicationTests {
 	}
 
 	@Test
-	public void testFormat(){
+	public void testFormat() {
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 		System.out.println(format.format(new Date()));
 	}
 
 	@Test
-	public void testCount() throws Exception{
-		CountDownLatch countDownLatch = new CountDownLatch(10000);
-		final int[] total = {0};
-		for(int i=0; i<10000; i++) {
+	public void testCount() throws Exception {
+		CountDownLatch countDownLatch = new CountDownLatch(100);
+		final int[] total = { 0 };
+		for (int i = 0; i < 100; i++) {
 			Thread thread = new Thread(() -> {
 				try {
 					Thread.sleep(100);
-				}catch (Exception e) {
+				} catch (Exception e) {
 
 				}
 				total[0]++;
@@ -221,7 +232,6 @@ public class CommonDemoApplicationTests {
 			System.out.println(entity.getName());
 		});
 
-
 		sort = "-displayName";
 		sortValue = "+displayName".equalsIgnoreCase(sort) ? 1 : ("-displayName".equalsIgnoreCase(sort) ? -1 : 0);
 		System.out.println("------desc-----");
@@ -231,9 +241,9 @@ public class CommonDemoApplicationTests {
 	}
 
 	@Test
-	public void testList(){
+	public void testList() {
 		List<UserEntity> list = new ArrayList<>();
-		for(int i=0; i<10; i++) {
+		for (int i = 0; i < 10; i++) {
 			UserEntity entity = new UserEntity();
 			entity.setId(1);
 			list.add(entity);
@@ -242,8 +252,69 @@ public class CommonDemoApplicationTests {
 	}
 
 	@Test
-	public void testLogger(){
+	public void testLogger() {
 		logger.debug("This is log {}", "HHH");
+	}
+
+	@Test
+	public void testWritePdf() throws Exception {
+		String classPath = this.getClass().getResource("/").getPath();
+		System.out.println(classPath);
+		File file = new File(classPath + "/test.pdf");
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		System.out.println(file.exists());
+		PDDocument document = PDDocument.load(file);
+		PDPage page = new PDPage();
+		document.addPage(page);
+		PDFont font = PDType1Font.HELVETICA_BOLD;
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
+		contentStream.beginText();
+		contentStream.setFont(font, 20);
+		contentStream.showText("You are overwriting an existing content, you should use the append mode. You are overwriting an existing content, you should use the append mode. You are overwriting an existing content, you should use the append mode. You are overwriting an existing content, you should use the append mode. You are overwriting an existing content, you should use the append mode.");
+		contentStream.endText();
+		contentStream.close();
+		document.save(file);
+		document.close();
+	}
+	
+	@Test
+	public void testAppendPdf() throws Exception {
+		String classPath = this.getClass().getResource("/").getPath();
+		System.out.println(classPath);
+		File file = new File(classPath + "/test.pdf");
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		PDDocument document = PDDocument.load(file);
+		PDPage page = document.getPage(0);
+//		document.addPage(page);
+		PDFont font = PDType1Font.HELVETICA_BOLD;
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
+		contentStream.beginText();
+		contentStream.setFont(font, 20);
+		contentStream.showText("Hello Pdfbox...");
+		contentStream.endText();
+		PDImageXObject image = PDImageXObject.createFromFile(classPath + "/simon.jpg", document);
+		contentStream.drawImage(image, 0, 50);
+		contentStream.close();
+		document.save(file);
+		document.close();
+	}
+
+	@Test
+	public void testReadPdf() throws Exception {
+		String classPath = this.getClass().getResource("/").getPath();
+		File file = new File(classPath + "/test.pdf");
+		PDDocument document = PDDocument.load(file);
+		PDFTextStripper textStripper = new PDFTextStripper();
+		System.out.println(textStripper.getText(document));
+	}
+	
+	@Test
+	public void testFop() throws Exception {
+		
 	}
 
 }
